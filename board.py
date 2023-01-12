2eb4ba470ee9736f09711dafc534e3d4d865209d
Amy Oppong <serwaa97@hotmail.com>
2023-01-12 14:00:04 +0100

    begin met het laden van csv bestand

--- board.py
+++ board.py
@@ -1,4 +1,23 @@
 class board(object):
     """ maakt een board aan"""
-    def __init__(self):
+    def __init__(self,filename):
         self.field = [[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]]
+        
+        self.load_board(f"gameboards/{filename}.csv")
+        
+    def load_board(self,filename):
+
+        with open(filename) as f:
+
+            while True:
+                line = f.readline()
+
+                if line == "\n":
+                    break
+
+                auto_data = line.split(',')
+                auto_letter = auto_data[0]
+                auto_dirc = auto_data[1]
+                auto_coords = (auto_data[2],auto_data[3])
+                auto_length = auto_data[4].rstrip('\n')
+                
---
+++ main.py
@@ -0,0 +1,19 @@
+from sys import argv
+from board import board
+import csv
+
+def create_output():
+    with open('output.csv','w',newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow("car","move")
+
+if __name__ == "__main__":
+    if len(argv) not in [1,2]:
+        print("Usage: python main.py [name]")
+        exit(1)
+
+    if len(argv) == 2:
+        filename = argv[1]
+        Board = board(filename)
+
+    
