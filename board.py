9426678eb9a0555b118a61dc42ef363ab58cc34f
ZoeyZorgvol <zoeyzorgvol@gmail.com>
2023-01-12 13:55:57 +0100

    board aangepast

--- board.py
+++ board.py
@@ -1,4 +1,20 @@
-class board(object):
+class Board(object):
     """ maakt een board aan"""
     def __init__(self):
-        self.field = [[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]]
+        self.board  = [[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0]]
+
+    def printboard(self):
+        for row in (0,8):
+            for column in (0,8):
+                if row == 0:
+                    print("1")
+                elif self.board[row][column] == [row][0]:
+                    print("1")
+                elif self.board[row][column] == [row][7]:
+                    print("1")
+                else:
+                     print("0")
+
+
+
+
