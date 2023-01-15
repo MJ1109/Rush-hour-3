e831b9a8024005243054a001e3726a20d6ea1327
ZoeyZorgvol <zoeyzorgvol@gmail.com>
2023-01-15 20:44:55 +0100

    board aangpast

--- board.py
+++ board.py
@@ -1,17 +1,33 @@
 class Board(object):
     """ maakt een board aan"""
     def __init__(self):
-        self.board  = [[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0]]
+        self.board = [[1,1,1,1,1,1,1,1],[1,0,0,0,0,0,0,1],[1,0,0,0,0,0,0,1],[1,0,0,0,0,0,0,1],[1,0,0,0,0,0,0,1],[1,0,0,0,0,0,0,1],[1,0,0,0,0,0,0,1],[1,1,1,1,1,1,1,1]]
 
-    def printboard(self):
-        for row in (0,8):
-            for column in (0,8):
-                if row == 0:
-                    print("1")
-                elif self.board[row][column] == [row][0]:
-                    print("1")
-                elif self.board[row][column] == [row][7]:
-                    print("1")
-                else:
-                     print("0")
+    def print_empty_board(self):
+        for row in range(8):
+            for column in range(8):
+                print(f" {self.board[row][column]} ", end='')
+            print()
                      
+
+
+
+        
+        self.load_board(f"gameboards/{filename}.csv")
+        
+    def load_board(self,filename):
+
+        with open(filename) as f:
+
+            while True:
+                line = f.readline()
+
+                if line == "\n":
+                    break
+
+                auto_data = line.split(',')
+                auto_letter = auto_data[0]
+                auto_dirc = auto_data[1]
+                auto_coords = (auto_data[2],auto_data[3])
+                auto_length = auto_data[4].rstrip('\n')
+                
