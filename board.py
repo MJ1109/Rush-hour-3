99e3b5e9810bf603beed934211e1a68d4c10e645
ZoeyZorgvol <zoeyzorgvol@gmail.com>
2023-01-16 09:35:00 +0100

    verandereingen in booard gemaakt en een cars class toegevoegd

--- board.py
+++ board.py
@@ -1,33 +1,24 @@
+from cars import Cars 
 class Board(object):
     """ maakt een board aan"""
     def __init__(self):
-        self.board = [[1,1,1,1,1,1,1,1],[1,0,0,0,0,0,0,1],[1,0,0,0,0,0,0,1],[1,0,0,0,0,0,0,1],[1,0,0,0,0,0,0,1],[1,0,0,0,0,0,0,1],[1,0,0,0,0,0,0,1],[1,1,1,1,1,1,1,1]]
+        self.board  = [[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0]]
 
     def print_empty_board(self):
-        for row in range(8):
-            for column in range(8):
+        for row in range(6):
+            for column in range(6):
                 print(f" {self.board[row][column]} ", end='')
             print()
-                     
 
+    def add_cars(self):
+        for cars_list in Cars.cars_list:
+            r = Cars.row
+            c = Cars.col
+            if Cars.orientation == "H":
+                for i in range(Cars.length):
+                    self.board[row][col + i] = Cars.letter
+            else:
+                for i in range(Cars.length):
+                    self.board[row+i][col] = Cars.letter
 
-
-        
-        self.load_board(f"gameboards/{filename}.csv")
-        
-    def load_board(self,filename):
-
-        with open(filename) as f:
-
-            while True:
-                line = f.readline()
-
-                if line == "\n":
-                    break
-
-                auto_data = line.split(',')
-                auto_letter = auto_data[0]
-                auto_dirc = auto_data[1]
-                auto_coords = (auto_data[2],auto_data[3])
-                auto_length = auto_data[4].rstrip('\n')
-                
+        return self.board    
---
+++ main.py
@@ -0,0 +1,29 @@
+class Cars(object):
+    """ maakt de auto's aan aan slaat ze op """
+    def __init__(self, car, orientation, column, row, length):
+        self.car_letter = car
+        self.orientation= orientation
+        self.col = column
+        self.row = row
+        self.lengt = length
+
+        self.cars_list = []
+
+    def load_cars(self, filename):
+        # laad de auto, orientatie, colomn, rij en lengte in
+        with open(filename) as f:
+            while True:
+                line = f.readline()
+
+                if line == "\n":
+                    break
+
+                splits = line.strip().splits(",")
+
+                auto_letter = splits[0]
+                # voegt de auto toe aan lijst met alleen de auto's
+                self.cars_list.append(auto_letter)
+
+                auto_dirc = splits[1]
+                auto_coords = (splits[2],splits[3])
+                auto_length = splits[4].rstrip('\n')
